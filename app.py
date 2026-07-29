ea870b7aadd6a28ff8384d3a2a4f3fb0764d8942
Pheew23 <106793294+Pheew23@users.noreply.github.com>
2026-07-29 11:24:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,7 +347,7 @@ with st.sidebar:
     st.divider()
     MODEL_NAME = st.selectbox(
         "🧠 Pilih Model AI:",
-        ["openai/gpt-oss-120b", "nvidia/nemotron-3-ultra-550b-a55b", "google/diffusiongemma-26b-a4b-it", "deepseek-ai/deepseek-v4-flash"],
+        ["thinkingmachines/inkling", "nvidia/nemotron-3-ultra-550b-a55b", "google/diffusiongemma-26b-a4b-it", "deepseek-ai/deepseek-v4-flash"],
         index=1
     )
 
